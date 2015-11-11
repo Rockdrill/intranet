76a962b8b5932c660a46bef5604919cd95c34df3
Luis Augusto Claudio Ponce <megabyte1507@gmail.com>
2015-11-10 19:03:44 -0500

    new logo

--- wp-content/themes/catch-responsive/functions.php
+++ wp-content/themes/catch-responsive/functions.php
@@ -25,10 +25,3 @@ require get_template_directory() . '/inc/catchresponsive-core.php';
 
 
 
-function my_custom_login_logo() {
-    echo '<style type="text/css">
-        h1 a { background-image:url('.get_bloginfo('template_directory').'/images/milogo.png) !important; }
-    </style>';
-}
-
-add_action('login_head', 'my_custom_login_logo');
